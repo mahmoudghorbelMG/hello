cdb86293c1f0142f80333f692ff1344410338771
mahmoudghorbelMG <68605873+mahmoudghorbelMG@users.noreply.github.com>
2021-11-29 12:18:29 +0100

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World! Mahmoud ya m3allim"
+		response = "Hello World! Mahmoud version"
 	}
 
 	fmt.Fprintln(w, response)
